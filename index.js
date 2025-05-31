70fd4348446186f2dba33956e94d7e99ad702975
IvanCedric0 <kouameivansich@gmail.com>
2025-05-31 10:21:10 +0000

    Make Homepage form display Itune API results in results page when cliked on

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
 import express from 'express';
+import bodyParser from 'body-parser'
+import axios from 'axios';
 
 const app = express();
 const port = 3000;
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", (req, res) =>{
     res.render("index.ejs")
+});
+
+app.get("/search/results", async(req, res) =>{
+    try{
+        const userInput = req.query.term;
+        console.log(userInput)
+        try {
+            const result = await axios.get(`https://itunes.apple.com/search?term=${userInput}`);
+            console.log(result.data.results); 
+            res.render("results.ejs", {data : result.data.results});
+        }catch (err){
+            console.log(err)
+        }
+        
+    } catch (err){
+        console.log(err)
+    }  
 });
 
 app.listen(port, () =>{
